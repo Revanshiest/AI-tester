4b78c7f7164ccdadca33b00c36814c580860bc1f
Revanshiest <emezhenov@gmail.com>
2025-09-23 03:39:37 +0600

    feat: Application Configuration constants

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+# Shared constants for the bot
+
+# Telegram message chunk size to avoid hitting message length limits
+MAX_TELEGRAM_CHUNK = 3800
+
+# Maximum number of messages to keep in in-memory history per user
+MAX_HISTORY_MESSAGES = 20
+
+# Auto-end session after this many seconds of inactivity
+INACTIVITY_TIMEOUT_SECONDS = 600
